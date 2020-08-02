d4ea72e5d2af14f5ebf4c8d0467483f10788081f
Yekon08 <max1901@live.fr>
2020-08-02 23:26:44 +0200

    Graphql query markdown

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,40 @@
 import React from 'react'
+import { graphql, useStaticQuery, Link } from 'gatsby'
 
 import Layout from '../components/Layout'
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            title
+                            date
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+    const displayPosts = data.allMarkdownRemark.edges.map(post => {
+        return (
+            <li>
+                <h2>{post.node.frontmatter.title}</h2>
+                <p>{post.node.frontmatter.date}</p>
+            </li>
+        )
+    })
+
     return(
         <Layout>
             <h1>Blog</h1>
             <p>Posts will show up here later on !</p>
+            <ol>
+                {displayPosts}
+            </ol>
         </Layout>
     )
 }
