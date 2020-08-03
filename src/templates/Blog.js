257b3ac9803c57b2b8b1e5ce38d0662854304499
Yekon08 <max1901@live.fr>
2020-08-03 10:08:13 +0200

    Automatic creation blog pages

---
+++ src/templates/Blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const Blog = () => {
+    return (
+        <Layout>
+            <p>Bonjour c'est le blog template</p>
+        </Layout>
+    )
+}
+
+export default Blog
